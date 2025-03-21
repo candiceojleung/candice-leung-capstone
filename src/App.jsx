66f3960c8298730e30263533b28cb881c66382f3
candiceojleung <candice.leung@live.ca>
2025-03-21 18:26:22 -0400

    add in additional route paths for new footer linked pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.scss'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage/HomePage';
 import ResourcesPage from './pages/ResourcesPage/ResourcesPage';
+import FAQs from './pages/FAQs/FAQs';
+import AboutUs from './pages/AboutUs/AboutUs';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 
@@ -13,6 +15,8 @@ function App() {
        <Routes>
        <Route path="/" element={<HomePage />} />
        <Route path="/resources" element={<ResourcesPage />} />
+       <Route path="/about" element={<AboutUs />} />
+       <Route path="/faqs" element={<FAQs />} />
        </Routes>
        <Footer/>
     </Router>
