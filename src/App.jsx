ef06fc80a7f19ca1f55139a04e850e73e4432d57
candiceojleung <candice.leung@live.ca>
2025-03-21 01:02:33 -0400

    add in footer and header

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import './App.scss'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage/HomePage';
 import ResourcesPage from './pages/ResourcesPage/ResourcesPage';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 function App() {
 
   return (
-    <Router>    
+    <Router>
+      <Header />
        <Routes>
        <Route path="/" element={<HomePage />} />
        <Route path="/resources" element={<ResourcesPage />} />
        </Routes>
+       <Footer/>
     </Router>
   )
 }
