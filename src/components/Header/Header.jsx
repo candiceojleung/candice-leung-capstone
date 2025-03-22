a73f90c3fd58f0e454ef068b3d06d7c67f170dac
candiceojleung <candice.leung@live.ca>
2025-03-22 18:29:04 -0400

    style header and make it responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,52 @@
-import { NavLink } from "react-router-dom";
+import { NavLink , Link} from "react-router-dom";
 import "./Header.scss";
 
 function Header() {
   return (
     <header className="header__container">
-    <h1 className="header__heading">periodic.ally</h1>
+      <Link to="/"><h1 className="header__heading">periodic.ally</h1></Link>
+      <nav className="header__nav">
+        <NavLink to="/"  className={({ isActive }) => 
+            isActive ? "header__link header__link--active" : "header__link"
+          }>
+          <img
+            className="header__icon"
+            src="../src/assets/icons/home.svg"
+            alt="home"
+          />
+          <p className="header__text header__text--adjust">homepage</p>
+        </NavLink>
+        <NavLink to="/about"  className={({ isActive }) => 
+            isActive ? "header__link header__link--active" : "header__link"
+          }>          <img
+            className="header__icon"
+            src="../src/assets/icons/about-us.svg"
+            alt="about-us icon"
+          />
+          <p className="header__text">about us</p>
+        </NavLink>
+        <NavLink to="/resources"  className={({ isActive }) => 
+            isActive ? "header__link header__link--active" : "header__link"
+          }>     
+          <img
+            className="header__icon"
+            src="../src/assets/icons/open-book.svg"
+            alt="resources icon"
+          />
+          <p className="header__text">resources</p>
+        </NavLink>
+        <NavLink to="/faqs"  className={({ isActive }) => 
+            isActive ? "header__link header__link--active" : "header__link"
+          }>     
+          <img
+            className="header__icon"
+            src="../src/assets/icons/faq.svg"
+            alt="frequently asked questions icon"
+          />
+          <p className="header__text">faq</p>
+        </NavLink>
+        <p className="header__text header__text--empty"></p>
+      </nav>
     </header>
   );
 }
