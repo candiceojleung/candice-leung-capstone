02d00d491d70ee2d0ab6032ad61b47cb595bc961
candiceojleung <candice.leung@live.ca>
2025-03-23 03:48:21 -0400

    add additional styling to form component

--- src/components/SymptomButtons/SymptomButtons.jsx
+++ src/components/SymptomButtons/SymptomButtons.jsx
@@ -3,7 +3,7 @@ import './SymptomButtons.scss';
 const SymptomButton = ({ symptom, isSelected, onClick }) => (
   <button
     type="button"
-    className={`symptom-button ${isSelected ? 'selected' : ''}`}
+    className={`symptom-button ${isSelected ? 'symptom-button--selected' : ''}`}
     onClick={() => onClick(symptom)}
   >
     {symptom.name}
